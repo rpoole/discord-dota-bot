9c73c75304b0ad912947788a3fd8215b71437343
Ralph Mancinelli <ralfizzle@gmail.com>
2017-02-06 22:49:24 -0500

    Adding Capistrano.

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+# config valid only for current version of Capistrano
+lock "3.7.2"
+
+require "capistrano/scm/git"
+
+set :default_stage, "production"
+
+set :application, "discord-dota-bot"
+set :repo_url, "git@github.com:ralfizzle/discord-dota-bot.git"
+
+namespace :deploy do
+	task :bundle_install do
+		on roles :all do
+	 		execute "cd #{release_path} && GOPATH=~/Projects/golang go build dota.go"
+		end
+	end
+end
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "~/Projects/golang/src/discord-dota-bot"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
